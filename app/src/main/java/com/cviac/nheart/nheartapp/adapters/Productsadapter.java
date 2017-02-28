fbd61a33a02110088919482fe073b65e7c71e009
cviacmob <mob@1234>
2017-02-28 10:43:38 +0530

    27/02

--- app/src/main/java/com/cviac/nheart/nheartapp/adapters/Productsadapter.java
+++ app/src/main/java/com/cviac/nheart/nheartapp/adapters/Productsadapter.java
@@ -11,10 +11,13 @@ import android.widget.TextView;
 import com.cviac.nheart.nheartapp.R;
 
 import com.cviac.nheart.nheartapp.datamodel.Product;
+import com.cviac.nheart.nheartapp.utilities.ImageLoader;
 import com.squareup.picasso.Picasso;
 
 
 import java.util.List;
+
+import static com.cviac.nheart.nheartapp.R.id.url;
 
 public class Productsadapter extends BaseAdapter {
 
@@ -78,8 +81,12 @@ public class Productsadapter extends BaseAdapter {
         holder.tv.setText(ct.getName());
         holder.price.setText(ct.getPrice());
         holder.price1.setText(ct.getSpecial());
+        int loader = R.drawable.heart;
         String url = ct.getThumb();
-        url = url.replace("localhost","192.168.1.133");
+        ImageLoader imgLoader = new ImageLoader( this.ctx );
+        imgLoader.DisplayImage(url, loader,holder.imgView);
+
+       // url = url.replace("localhost","192.168.1.133");
         try {
             Picasso.with(vw.getContext()).load(url).resize(500,500).into( holder.imgView);
 //            holder.imgView.setImageURI(Uri.parse(url));
---
+++ app/src/main/java/com/cviac/nheart/nheartapp/utilities/Utils.java
@@ -0,0 +1,27 @@
+package com.cviac.nheart.nheartapp.utilities;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by admin1 on 2/27/2017.
+ */
+
+public class Utils {
+    public static void CopyStream(InputStream is, OutputStream os)
+    {
+        final int buffer_size=1024;
+        try
+        {
+            byte[] bytes=new byte[buffer_size];
+            for(;;)
+            {
+                int count=is.read(bytes, 0, buffer_size);
+                if(count==-1)
+                    break;
+                os.write(bytes, 0, count);
+            }
+        }
+        catch(Exception ex){}
+    }
+}
